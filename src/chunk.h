559c75ca65ab49e3a109f2f0adaac64c38e635ef
Kevin Matthes <kevin.matthes@s2019.tu-chemnitz.de>
2022-03-02 20:51:27 +0100

    Add brief description

--- src/chunk.h
+++ src/chunk.h
@@ -18,13 +18,15 @@
 
 /**
  * \author      Kevin Matthes
- * \brief       .
+ * \brief       The bytecode utilities.
  * \copyright   (C) 2022 Kevin Matthes.
  *              This file is licensed GPL 2 as of June 1991.
  * \date        2022
  * \file        chunk.h
  * \note        See `LICENSE' for full license.
  *              See `README.md' for project details.
+ *
+ * This file introduces the bytecode utilities.
  */
 
 /******************************************************************************/
@@ -34,8 +36,8 @@
  */
 
 #pragma once
-#ifndef ___H__
-#define ___H__
+#ifndef __LOX__CHUNK_H__
+#define __LOX__CHUNK_H__
 
 
 
@@ -80,6 +82,6 @@
 #endif  // ! __LOX_INTERNAL__
 
 // Leaving the header.
-#endif  // ! ___H__
+#endif  // ! __LOX__CHUNK_H__
 
 /******************************************************************************/
