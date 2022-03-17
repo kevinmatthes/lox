6974739ba1309ad2cb080148ad1462d924cc8cff
Kevin Matthes <kevin.matthes@s2019.tu-chemnitz.de>
2022-03-17 18:49:43 +0100

    Bugfix

--- liblox/debug_disassemble_chunk.c
+++ liblox/debug_disassemble_chunk.c
@@ -22,7 +22,7 @@
  * \copyright   (C) 2022 Kevin Matthes.
  *              This file is licensed GPL 2 as of June 1991.
  * \date        2022
- * \file        debug.c
+ * \file        debug_disassemble_chunk.c
  * \note        See `LICENSE' for full license.
  *              See `README.md' for project details.
  *
@@ -60,96 +60,6 @@ void debug_disassemble_chunk ( const chunk_t * const    chunk
         );
 
     return;
-}
-
-
-
-/**
- * \brief   Determine the offset for a constant instruction.
- * \param   name    The instruction's identifier.
- * \param   chunk   The chunk to disassemble.
- * \param   offset  The index of the given instruction.
- * \return  The index of the succeeding instruction.
- *
- * A constant instruction contains the opcode as well as the corresponding value
- * to store.
- */
-
-int debug_constant_instruction ( const char * const       name
-                               , const chunk_t * const    chunk
-                               , const int                offset
-                               )
-{
-    const uint8_t constant = chunk -> code[offset + 0x1];
-
-    fprintf (stderr, "%-16s %4x '", name, constant);
-    value_print (chunk -> constants.values[constant], stderr);
-    fprintf (stderr, "'\n");
-
-    return offset + 0x2;
-}
-
-
-
-/**
- * \brief   Explain the purpose of a certain instruction.
- * \param   chunk   The chunk to analyse the instructions of.
- * \param   offset  The index of the intruction to examine.
- * \return  The index of the next instruction.
- *
- * This function will prompt the purpose of the instructions a certain sequence
- * of bytecode contains to the user.
- */
-
-int debug_disassemble_instruction ( const chunk_t * const   chunk
-                                  , const int               offset
-                                  )
-{
-    const uint8_t   instruction = chunk -> code[offset];
-    int             ret         = 0x0;
-
-    fprintf (stderr, "%04x ", offset);
-
-    if (offset && chunk -> lines[offset] == chunk -> lines[offset - 0x1])
-        fprintf (stderr, "   | ");
-    else
-        fprintf (stderr, "%4d ", chunk -> lines[offset]);
-
-    switch (instruction)
-    {
-        default:
-            fprintf (stderr, "Unknown opcode %d.\n", instruction);
-            ret = 0x1;
-            break;
-
-        case OP_CONSTANT:
-            ret = debug_constant_instruction ("OP_CONSTANT", chunk, offset);
-            break;
-
-        case OP_RETURN:
-            ret = debug_simple_instruction ("OP_RETURN", offset);
-            break;
-    };
-
-    return ret;
-}
-
-
-
-/**
- * \brief   Determine the offset for a simple instruction.
- * \param   name    The instruction's identifier.
- * \param   offset  The index of the given instruction.
- * \return  The index of the succeeding instruction.
- *
- * A simple instruction is an operation on its own which does not accept any
- * operands.  Hence, the offset to its successing operation is always 1.
- */
-
-inline int debug_simple_instruction (const char * const name, const int offset)
-{
-    fprintf (stderr, "%s\n", name);
-    return offset + 0x1;
 }
 
 /******************************************************************************/
