c77786d33947bfec3f7c2a695597bedf6314ec1f
Kevin Matthes <kevin.matthes@s2019.tu-chemnitz.de>
2022-03-17 18:59:47 +0100

    Bugfix

--- liblox/value_print.c
+++ liblox/value_print.c
@@ -22,7 +22,7 @@
  * \copyright   (C) 2022 Kevin Matthes.
  *              This file is licensed GPL 2 as of June 1991.
  * \date        2022
- * \file        value.c
+ * \file        value_print.c
  * \note        See `LICENSE' for full license.
  *              See `README.md' for project details.
  *
@@ -37,74 +37,6 @@
 
 // This project.
 #include "value.h"
-
-
-
-/**
- * \brief   Free a value array.
- * \param   array   The array to free.
- *
- * Since the given array required the allocation of memory, this allocation
- * should be revoked appropriately as soon as the array is not required anymore.
- *
- * After the freeing is complete, the array will be reinitialised.
- */
-
-void value_array_free (value_array_t * const array)
-{
-    FREE_ARRAY (value_t, array -> values, array -> capacity);
-    value_array_init (array);
-
-    return;
-}
-
-
-
-/**
- * \brief   Construct a new value array.
- * \param   array   The array to initialise.
- *
- * This function will prepare the given array with sane initial values.
- */
-
-void value_array_init (value_array_t * array)
-{
-    array -> capacity   = 0x0;
-    array -> count      = 0x0;
-    array -> values     = NULL;
-
-    return;
-}
-
-
-
-/**
- * \brief   Append a value to a certain value array.
- * \param   array   The array to append a value to.
- * \param   value   The value to append.
- *
- * This function will add a given value to a certain value array.
- */
-
-void value_array_write (value_array_t * const array, const value_t value)
-{
-    if (array -> capacity < array -> count + 0x1)
-    {
-        const int old_capacity = array -> capacity;
-
-        array -> capacity   = GROW_CAPACITY (old_capacity);
-        array -> values     = GROW_ARRAY ( value_t
-                                         , array -> values
-                                         , old_capacity
-                                         , array -> capacity
-                                         );
-    };
-
-    array -> values[array -> count] = value;
-    array -> count++;
-
-    return;
-}
 
 
 
