1e4743d6c3131eb56d1a17c6bcae31aea67262ae
Kevin Matthes <kevin.matthes@s2019.tu-chemnitz.de>
2022-03-17 18:17:20 +0100

    Duplicate source file

---
+++ liblox/chunk_init.c
@@ -0,0 +1,140 @@
+/*
+ * Copyright (C) 2022 Kevin Matthes
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+/**
+ * \author      Kevin Matthes
+ * \brief       The bytecode utilities.
+ * \copyright   (C) 2022 Kevin Matthes.
+ *              This file is licensed GPL 2 as of June 1991.
+ * \date        2022
+ * \file        chunk.c
+ * \note        See `LICENSE' for full license.
+ *              See `README.md' for project details.
+ *
+ * This file implements the bytecode utilities.
+ */
+
+/******************************************************************************/
+
+/*
+ * Includes.
+ */
+
+// This project.
+#include "chunk.h"
+
+
+
+/**
+ * \brief   Append a constant to a bytecode chunk's value array.
+ * \param   chunk   The chunk to append a constant to.
+ * \param   value   The value to add to the chunk.
+ * \return  The index of the constant just added.
+ *
+ * This function will add a given constant to a certain chunk of bytecode.
+ */
+
+inline int chunk_add_const (chunk_t * const chunk, const value_t value)
+{
+    value_array_write (& chunk -> constants, value);
+    return chunk -> constants.count - 0x1;
+}
+
+
+
+/**
+ * \brief   Free a chunk of bytecode.
+ * \param   chunk   The chunk to free.
+ *
+ * Since the given chunk required the allocation of memory, this allocation
+ * should be revoked appropriately as soon as the chunk is not required anymore.
+ *
+ * After the freeing is complete, the chunk will be reinitialised.
+ */
+
+void chunk_free (chunk_t * chunk)
+{
+    FREE_ARRAY (uint8_t, chunk -> code, chunk -> capacity);
+    FREE_ARRAY (int, chunk -> lines, chunk -> capacity);
+    value_array_free (& chunk -> constants);
+    chunk_init (chunk);
+
+    return;
+}
+
+
+
+/**
+ * \brief   Construct a new chunk of bytecode.
+ * \param   chunk   The chunk to initialise.
+ *
+ * This function will prepare the given chunk of bytecode with sane initial
+ * values.
+ */
+
+void chunk_init (chunk_t * const chunk)
+{
+    chunk -> capacity   = 0x0;
+    chunk -> code       = NULL;
+    chunk -> count      = 0x0;
+    chunk -> lines      = NULL;
+
+    value_array_init (& chunk -> constants);
+
+    return;
+}
+
+
+
+/**
+ * \brief   Append an instruction to a certain chunk of bytecode.
+ * \param   chunk   The chunk to append a byte to.
+ * \param   byte    The byte to append to the chunk.
+ * \param   line    The line where this instruction occurs.
+ *
+ * This function will add a given instruction to a certain chunk of bytecode.
+ */
+
+void chunk_write (chunk_t * const chunk, const uint8_t byte, const int line)
+{
+    if (chunk -> capacity < chunk -> count + 0x1)
+    {
+        const int old_capacity = chunk -> capacity;
+
+        chunk -> capacity   = GROW_CAPACITY (old_capacity);
+        chunk -> code       = GROW_ARRAY ( uint8_t
+                                         , chunk -> code
+                                         , old_capacity
+                                         , chunk -> capacity
+                                         );
+        chunk -> lines      = GROW_ARRAY ( int
+                                         , chunk -> lines
+                                         , old_capacity
+                                         , chunk -> capacity
+                                         );
+    };
+
+    chunk -> code[chunk -> count]   = byte;
+    chunk -> lines[chunk -> count]  = line;
+
+    chunk -> count++;
+
+    return;
+}
+
+/******************************************************************************/
