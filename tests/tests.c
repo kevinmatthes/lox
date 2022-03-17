f744c4cc0589f4556f23c6b5c8e50427907a7602
Kevin Matthes <kevin.matthes@s2019.tu-chemnitz.de>
2022-03-17 18:21:01 +0100

    Bugfix

--- tests/tests.c
+++ tests/tests.c
@@ -22,7 +22,7 @@
  * \copyright   (C) 2022 Kevin Matthes.
  *              This file is licensed GPL 2 as of June 1991.
  * \date        2022
- * \file        main.c
+ * \file        tests.c
  * \note        See `LICENSE' for full license.
  *              See `README.md' for project details.
  *
